69208182e076076f31fd568398586c44434b4a35
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-05-03 22:33:19 -0700

    create a diff server on port 3000

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+//app.listen(3000);
 
 // create a server with the express app as a listener
 var server = http.createServer(app).listen(9000);
@@ -18,6 +19,7 @@ var binaryserver = new BinaryServer({server: server, path: '/binary-endpoint'});
 binaryserver.on('connection', function(client){
 	// Incoming stream from browsers
 	client.on('stream', function(stream, meta){
+		console.log('Streaming from Server......', meta);
 		var file = fs.createWriteStream(__dirname+ '/public/' + meta.name);
 		stream.pipe(file);
 	})
